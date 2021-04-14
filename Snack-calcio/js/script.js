554d1dcf4ef2a965b4d84f91d263b8d405f36009
karin valcanover <valcanoverkarin@outlook.it>
2021-04-14 16:09:13 +0200

    creato array squadre

--- Snack-calcio/js/script.js
+++ Snack-calcio/js/script.js
@@ -4,3 +4,32 @@
 //2. Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti. 
 //3. Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e 
 //4. stampiamo tutto in console
+
+//1a. creo un array di oggetti, ogni oggetto descriverà il nome di una squadra di calcio mentre i punti fatti e i falli subiti sono settati a 0
+const squadre = [
+    {
+        //squadra 0
+        'nome' : 'Ascoli',
+        'puntifatti' : 0,
+        'fallisubiti' : 0
+    },
+    {
+        //squadra 1
+        'nome' : 'Reggiana',
+        'puntifatti' : 0,
+        'fallisubiti' : 0
+    },
+    {
+        //squadra 2
+        'nome' : 'Cosenza',
+        'puntifatti' : 0,
+        'fallisubiti' : 0
+    },
+    {
+        //squadra 3
+        'nome' : 'Pordenone',
+        'puntifatti' : 0,
+        'fallisubiti' : 0
+    }
+];
+//console.log(squadre);
