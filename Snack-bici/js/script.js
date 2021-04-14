d0b60b349635a349da749474784d98af626931b6
karin valcanover <valcanoverkarin@outlook.it>
2021-04-14 15:19:25 +0200

    html js consegne esercizi

---
+++ Snack-bici/js/script.js
@@ -0,0 +1,3 @@
+//1. Creare un array di oggetti: 
+//   Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. 
+//2. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal Snack
