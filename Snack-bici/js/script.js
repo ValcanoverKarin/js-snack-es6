cd6036c967858f440956b407a832b289c2c7e27d
karin valcanover <valcanoverkarin@outlook.it>
2021-04-14 15:30:23 +0200

    creo array biciclette

--- Snack-bici/js/script.js
+++ Snack-bici/js/script.js
@@ -1,3 +1,30 @@
 //1. Creare un array di oggetti: 
 //   Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. 
 //2. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal Snack
+
+//1a. creo un array di oggetti, ogni oggetto descriverà il nome e il peso di una bici
+const biciclette = [
+    {
+        //bicicletta 0
+        'nome': 'Bianchini',
+        'peso': 5
+    },
+    {
+        //bicicletta 1
+        'nome': 'Olmo',
+        'peso': 12
+    },
+    {
+        //bicicletta 2
+        'nome': 'Venom',
+        'peso': 6
+    },
+    {
+        //bicicletta 3
+        'nome': 'Trek',
+        'peso': 8
+    }
+]
+
+//2a. imposto la bicicletta piu leggera come oggetto vuoto
+let biciclettaPiuLeggera = biciclette[0];
